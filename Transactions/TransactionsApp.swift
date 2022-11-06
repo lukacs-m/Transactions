9e392ffafa5773d10d7a7b79cac39eeeb487cd7d
mlukacs <mlukacs@deezer.com>
2022-11-06 22:11:05 +0100

    Initial Commit

---
+++ Transactions/TransactionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TransactionsApp.swift
+//  Transactions
+//
+//  Created by Martin Lukacs on 06/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct TransactionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
