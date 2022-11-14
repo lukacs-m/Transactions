0e27464e04926833ce49f73e2f85114a8eaa8dce
mlukacs <mlukacs@deezer.com>
2022-11-14 23:46:12 +0100

    added

---
+++ Packages/Test/Sources/Test/Test.swift
@@ -0,0 +1,10 @@
+public struct Test {
+    public private(set) var text = "Hello, World!"
+    public private(set) var text2 = "Hello, World!"
+    public private(set) var text3 = "Hello, World!"
+    public private(set) var text4 = "Hello, World!"
+    public private(set) var text5 = "Hello, Worldasasdada!"
+
+    public init() {
+    }
+}
